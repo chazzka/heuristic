69880a0efeaa56f5203b0e126b742d500b8cc5b3
chazzka <adamo.ulrich@gmail.com>
2021-04-10 01:21:19 +0200

    refactored new PSO

--- alg/pso.cpp
+++ alg/pso.cpp
@@ -1,7 +1,14 @@
 #include <vector>
 #include <iostream>
 #include <random>
+#include <cmath>
 #include "../cec20_test_func.cpp"
+
+//vars
+const int popSize = 25;
+int D, TF;
+//nebo 50, hlavně ať je to dělitelné 5000 - experimentuj a dívej se jak to vychází
+double c1 = 1.496180, c2 = c1, wStart = 0.9, wEnd = 0.4, vMax = 0.2, w = wStart, BOUNDARY_LOW, BOUNDARY_UP;
 
 struct result
 {
@@ -38,39 +45,18 @@ double generateRandomDouble(double min, double max)
     return (max - min) * ((double)rand() / (double)RAND_MAX) + min;
 }
 
-std::vector<result> run(int dimension, int testFunction, int boundaryLow, int boundaryUp)
+void initPopulation(std::vector<Particle> &population, std::vector<double> &leadingPosG, double gCost)
 {
-    //vars
-    int MAX_FEZ = 5000 * dimension;
-    std::vector<result> best_results;
-    int popSize = 25; //nebo 50, hlavně ať je to dělitelné 5000 - experimentuj a dívej se jak to vychází
-    int generations = MAX_FEZ / popSize;
-    double c1 = 1.496180; //Engelbrecth
-    double c2 = c1;
-    double wStart = 0.9;
-    double wEnd = 0.4;
-    double vMax = 0.2;
-    double w = wStart;
-    int fezCounter = 0;
-
-    std::vector<Particle> population;
-    //leader
-    std::vector<double> leadingPosG = generateRandomRange(dimension, boundaryLow, boundaryUp);
-    double gCost = 0;
-    cec20_test_func(leadingPosG.data(), &gCost, dimension, 1, testFunction);
-
     for (int i = 0; i < popSize; i++)
     {
-        std::vector<double> randomPosition = generateRandomRange(dimension, boundaryLow, boundaryUp);
-        //Initialize the particle's position with a uniformly distributed random vector xi ~ U(blo, bup)
+        std::vector<double> randomPosition = generateRandomRange(D, BOUNDARY_LOW, BOUNDARY_UP);
+        //Initialize the particle's position with a uniformly distriBOUNDARY_UPted random vector xi ~ U(BOUNDARY_LOW, BOUNDARY_UPp)
         //TODO initial velocity 0?
-        //Initialize the particle's velocity: vi ~ U(-|bup-blo|, |bup-blo|)
+        //Initialize the particle's velocity: vi ~ U(-|BOUNDARY_UPp-BOUNDARY_LOW|, |BOUNDARY_UPp-BOUNDARY_LOW|)
         //Initialize the particle's best known position to its initial position: pi ← xi
-        Particle p = {randomPosition, generateRandomRange(dimension, -(boundaryUp - boundaryLow), (boundaryUp - boundaryLow)), randomPosition};
+        Particle p = {randomPosition, generateRandomRange(D, 0, 0), randomPosition};
         double pCost = 0;
-        cec20_test_func(p.pBestPi.data(), &pCost, dimension, 1, testFunction);
-        //fezCounter++;
-        //ulozim si tady jeste cost a pak dole nemusim delat dalsi evaluaci
+        cec20_test_func(p.pBestPi.data(), &pCost, D, 1, TF);
         p.pBestCost = pCost;
         //find best swarm position and cost
         if (pCost < gCost)
@@ -81,69 +67,68 @@ std::vector<result> run(int dimension, int testFunction, int boundaryLow, int bo
         }
         population.push_back(p);
     }
+}
+
+void backToBoundaries(Particle &particle, int iteration)
+{
+    if (BOUNDARY_LOW > particle.positionXi[iteration] || particle.positionXi[iteration] > BOUNDARY_UP)
+    {
+        double randomNum = generateRandomDouble((double)BOUNDARY_LOW, (double)BOUNDARY_UP);
+        particle.positionXi[iteration] = randomNum;
+    }
+}
+
+std::vector<result> run(int dimension, int testFunction, int boundaryLow, int boundaryUp)
+{
+    D = dimension;
+    TF = testFunction;
+    BOUNDARY_LOW = boundaryLow;
+    BOUNDARY_UP = boundaryUp;
+    int MAX_FEZ = 5000 * dimension;
+    int generations = MAX_FEZ / popSize;
+
+    std::vector<result> best_results;
+    int fezCounter = 0;
+    std::vector<Particle> population;
+    std::vector<double> leadingPosG = generateRandomRange(dimension, boundaryLow, boundaryUp);
+    double gCost = 0;
+    cec20_test_func(leadingPosG.data(), &gCost, dimension, 1, testFunction);
+
+    initPopulation(population, leadingPosG, gCost);
 
     for (int g = 0; g < generations; g++)
     {
         for (int i = 0; i < population.size(); i++)
         {
+            Particle &currentParticle = population[i];
+
             for (int d = 0; d < dimension; d++)
             {
                 double rp = generateRandomDouble(0, 1);
                 double rg = generateRandomDouble(0, 1);
                 //Update the particle's velocity
-                double potentialVectorD = w * population[i].velocityVectorVi[d] + c1 * rp * (population[i].pBestPi[d] - population[i].positionXi[d]) + c2 * rg * (leadingPosG[d] - population[i].positionXi[d]);
+                double potentialVectorD = w * currentParticle.velocityVectorVi[d] + c1 * rp * (currentParticle.pBestPi[d] - currentParticle.positionXi[d]) + c2 * rg * (leadingPosG[d] - currentParticle.positionXi[d]);
 
-                //TODO: zkontrolovat
-                //musíš zkontrolovat jestli ten nový velocity vector není větší než 0.2*200 (vMax * |-100-100|) - pozor bounds je vektor, každá dimenze může mít jiný rozsah, bound[d]
-                //pozor velikost vektoru může být negativní (takže musíš kontrolvat i vektor co míří na druhou stranu - mínusový musí pak ale mířit zas do mínusu!)
-                //pozor, výsledek může být 40 ale i -40
-                if (abs(potentialVectorD) > vMax * (boundaryUp - boundaryLow))
-                {
-                    if (potentialVectorD < 0)
-                    {
-                        population[i].velocityVectorVi[d] = -potentialVectorD;
-                    }
-                    else
-                    {
-                        population[i].velocityVectorVi[d] = potentialVectorD;
-                    }
-                }
-                else
-                {
-                    if (potentialVectorD < 0)
-                    {
-                        population[i].velocityVectorVi[d] = -vMax;
-                    }
-                    else
-                    {
-                        population[i].velocityVectorVi[d] = vMax;
-                    }
-                }
-
+                currentParticle.velocityVectorVi[d] = fmax(fmin(potentialVectorD, boundaryUp), boundaryLow);
                 //Update the particle's position
-                population[i].positionXi[d] = population[i].positionXi[d] + population[i].velocityVectorVi[d];
+                currentParticle.positionXi[d] = currentParticle.positionXi[d] + currentParticle.velocityVectorVi[d];
 
-                //boundary check
-                if (boundaryLow > population[i].positionXi[d] || population[i].positionXi[d] > boundaryUp)
-                {
-                    double randomNum = generateRandomDouble((double)boundaryLow, (double)boundaryUp);
-                    population[i].positionXi[d] = randomNum;
-                }
+                backToBoundaries(currentParticle, d);
             }
             double newXiCost = 0;
-            cec20_test_func(population[i].positionXi.data(), &newXiCost, dimension, 1, testFunction);
+            cec20_test_func(currentParticle.positionXi.data(), &newXiCost, dimension, 1, testFunction);
             fezCounter++;
 
-            if (newXiCost < population[i].pBestCost)
+            if (newXiCost < currentParticle.pBestCost)
             {
                 //Update the particle's best known position
-                population[i].pBestPi = population[i].positionXi;
-                population[i].pBestCost = newXiCost;
+                currentParticle.pBestPi = currentParticle.positionXi;
+                currentParticle.pBestCost = newXiCost;
                 if (newXiCost < gCost)
                 {
                     //Update the swarm's best known position
                     gCost = newXiCost;
-                    leadingPosG = population[i].positionXi;
+                    leadingPosG = currentParticle.positionXi;
                 }
             }
 
